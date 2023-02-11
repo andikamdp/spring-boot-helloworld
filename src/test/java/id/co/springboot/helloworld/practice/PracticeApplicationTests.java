480d3066c21654f46d017a447b9b163046d3d9ce
andika mulyawan <dikaprapmanto@gmail.com>
2023-02-11 15:58:43 +0700

    init project

---
+++ src/test/java/id/co/springboot/helloworld/practice/PracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package id.co.springboot.helloworld.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
